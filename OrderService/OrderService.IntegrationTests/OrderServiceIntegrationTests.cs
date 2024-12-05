110ee26a0691186fab46d57e92b0a697b8bebab3
zola191 <zola19@yandex.ru>
2024-12-05 18:18:02 +0300

    +

---
+++ OrderService/OrderService.IntegrationTests/OrderServiceIntegrationTests.cs
@@ -0,0 +1,16 @@
+namespace OrderService.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void CreateOrder_WhenCatalogServiceReturnsSuccess_ShouldCreateOrderSuccessfully()
+    {
+        Assert.Pass();
+    }
+}
