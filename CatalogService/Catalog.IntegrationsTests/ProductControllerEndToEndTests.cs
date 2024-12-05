7b4ea72fd575958083b8bcf07dbb5c3dfa97259f
manalexov <manalexovik@yandex.ru>
2024-12-05 10:41:25 +0300

    Убрал миграции сделал singleton для context

--- CatalogService/Catalog.IntegrationsTests/ProductControllerEndToEndTests.cs
+++ CatalogService/Catalog.IntegrationsTests/ProductControllerEndToEndTests.cs
@@ -6,136 +6,115 @@ using Application.Models;
 using AutoFixture;
 using Domain.Abstractions;
 using Domain.Entities;
-using MediatR;
-using MediatR.Pipeline;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OrderManagementSystem.Infrastructure;
 using OrderManagementSystem.Infrastructure.Repository;
 
-namespace IntegrationsTests;
-
-public class ProductControllerEndToEndTests : IDisposable
+namespace IntegrationsTests
 {
-    private WebApplicationFactory<Program> _webHost;
-    private IServiceScope _serviceScope;
-    private HttpClient _client;
-    private IProductRepository _productRepository;
-    private ICategoryRepository _categoryRepository;
-    private IMediator _mediator;
-
-    [SetUp]
-    public void SetupForController()
+    public class ProductControllerEndToEndTests : IDisposable
     {
-        _webHost = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureServices(services =>
+        private WebApplicationFactory<Program> _webHost;
+        private HttpClient _client;
+        private CatalogDbContext _context;
+
+        [SetUp]
+        public void SetupForController()
+        {
+            _webHost = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
                 {
-                    var dbDescriptor = services.SingleOrDefault(f => f.ServiceType == typeof(CatalogDbContext));
-                    if (dbDescriptor != null)
+                    builder.ConfigureServices(services =>
                     {
-                        services.Remove(dbDescriptor);
-                    }
+                        var descriptors = services.Where(d => 
+                                d.ServiceType == typeof(CatalogDbContext) ||
+                                d.ServiceType == typeof(IProductRepository) ||
+                                d.ServiceType == typeof(ICategoryRepository))
+                            .ToList();
 
-                    var productRepoDescriptor = services.SingleOrDefault(f => f.ServiceType == typeof(IProductRepository));
-                    if (productRepoDescriptor != null)
-                    {
-                        services.Remove(productRepoDescriptor);
-                    }
+                        foreach (var descriptor in descriptors)
+                        {
+                            services.Remove(descriptor);
+                        }
 
-                    var categoryRepoDescriptor = services.SingleOrDefault(f => f.ServiceType == typeof(ICategoryRepository));
-                    if (categoryRepoDescriptor != null)
-                    {
-                        services.Remove(categoryRepoDescriptor);
-                    }
-                    
-                    services.AddDbContext<CatalogDbContext>(options =>
-                    {
-                        options.UseInMemoryDatabase("InMemoryCatalogDB");
-                    });
+                        var options = new DbContextOptionsBuilder<CatalogDbContext>()
+                            .UseInMemoryDatabase("InMemoryCatalogDB")
+                            .Options;
 
-                    services.AddScoped<IProductRepository, ProductRepository>();
-                    services.AddScoped<ICategoryRepository, CategoryRepository>();
+                        _context = new CatalogDbContext(options);
+                        services.AddSingleton(_ => _context);
+                        services.AddScoped<IProductRepository, ProductRepository>();
+                        services.AddScoped<ICategoryRepository, CategoryRepository>();
 
-                    services.AddMediatR(configuration =>
-                    {
-                        configuration.RegisterServicesFromAssemblyContaining<CreateProductCommand>();
+                        services.AddMediatR(configuration =>
+                        {
+                            configuration.RegisterServicesFromAssemblyContaining<CreateProductCommand>();
+                        });
                     });
                 });
-            });
 
-        _client = _webHost.CreateClient();
-        _serviceScope = _webHost.Services.CreateScope();
+            _client = _webHost.CreateClient();
 
-        var context = _serviceScope.ServiceProvider.GetRequiredService<CatalogDbContext>();
-        _productRepository = _serviceScope.ServiceProvider.GetRequiredService<IProductRepository>();
-        _categoryRepository = _serviceScope.ServiceProvider.GetRequiredService<ICategoryRepository>();
-        _mediator = _serviceScope.ServiceProvider.GetRequiredService<IMediator>();
+            _context.Products.RemoveRange(_context.Products);
+            _context.Categories.RemoveRange(_context.Categories);
+            _context.SaveChanges();
+        }
 
-        context.Products.RemoveRange(context.Products);
-        context.Categories.RemoveRange(context.Categories);
-        context.SaveChanges();
-    }
-    [Test]
-    public async Task CreateProduct_ReturnsCreatedResponse()
-    {
-        //не получается решить проблему обращения к разным БД в handler и в тесте
-        
-        var fixture = new Fixture();
-        fixture.Customize<DateTime>(c => c.FromFactory(() => DateTime.UtcNow));
-        fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
-        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        [Test]
+        public async Task CreateProduct_ReturnsCreatedResponse()
+        {
+            var fixture = new Fixture();
+            fixture.Customize<DateTime>(c => c.FromFactory(() => DateTime.UtcNow));
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
-        var categoryModelDto = fixture.CreateMany<CategoryModelDto>(2).ToList();
-        fixture.Customize<CreateProductRequest>(c => c
+            var categoryModelDtos = fixture.CreateMany<CategoryModelDto>(2).ToList();
+            var request = fixture.Build<CreateProductRequest>()
                 .With(req => req.Price, fixture.Create<decimal>() % 100 + 1)
                 .With(req => req.Name, fixture.Create<string>().Substring(0, 10))
                 .With(req => req.Description, fixture.Create<string>().Substring(0, 10))
                 .With(req => req.Quantity, fixture.Create<int>() % 100)
-                .With(req => req.CategoryModelDtos, fixture.CreateMany<CategoryModelDto>(2).ToList())
-        );
-        
-        var request = fixture.Create<CreateProductRequest>();
-        
-        var categories = categoryModelDto
-            .Select(dto => fixture.Build<Category>()
-                .With(c => c.Id, dto.Id)
-                .Create())
-            .ToList();
+                .With(req => req.CategoryModelDtos, categoryModelDtos)
+                .Create();
 
-        var context = _serviceScope.ServiceProvider.GetRequiredService<CatalogDbContext>();
-        await context.Categories.AddRangeAsync(categories);
-        await context.SaveChangesAsync();
-        
-        Assert.That(await context.Categories.CountAsync(), Is.EqualTo(categories.Count));
-        Console.WriteLine($"Context in test: {context.GetHashCode()}");
-        
-        var response = await _client.PostAsJsonAsync("/catalog", request);
-        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+            var categories = categoryModelDtos
+                .Select(dto => fixture.Build<Category>()
+                    .With(c => c.Id, dto.Id)
+                    .Create())
+                .ToList();
 
-        var productInDb = await context.Products.Include(product => product.Categories)
-            .FirstOrDefaultAsync(f => f.Name == request.Name);
-        
-        Assert.NotNull(productInDb);
-        Assert.That(productInDb.Name, Is.EqualTo(request.Name));
-        Assert.That(productInDb.Description, Is.EqualTo(request.Description));
-        Assert.That(productInDb.Price, Is.EqualTo(request.Price));
-        Assert.That(productInDb.Quantity, Is.EqualTo(request.Quantity));
+            await _context.Categories.AddRangeAsync(categories);
+            await _context.SaveChangesAsync();
 
-        var categoriesInDb = productInDb.Categories.Select(c => c.Name).ToList();
-        CollectionAssert.AreEquivalent(request.CategoryModelDtos.Select(c => c.Id), categoriesInDb);
-    }
+            Assert.That(await _context.Categories.CountAsync(), Is.EqualTo(categories.Count));
 
-    [TearDown]
-    public void TearDown()
-    {
-        _webHost.Dispose();
-    }
+            var response = await _client.PostAsJsonAsync("/catalog", request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
-    public void Dispose()
-    {
-        TearDown();
+            var productInDb = await _context.Products.Include(product => product.Categories)
+                .FirstOrDefaultAsync(f => f.Name == request.Name);
+
+            Assert.NotNull(productInDb);
+            Assert.That(productInDb.Name, Is.EqualTo(request.Name));
+            Assert.That(productInDb.Description, Is.EqualTo(request.Description));
+            Assert.That(productInDb.Price, Is.EqualTo(request.Price));
+            Assert.That(productInDb.Quantity, Is.EqualTo(request.Quantity));
+
+            var categoriesInDb = productInDb.Categories.Select(c => c.Name).ToList();
+            CollectionAssert.AreEquivalent(request.CategoryModelDtos.Select(c => c.Id), categoriesInDb);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webHost.Dispose();
+        }
+
+        public void Dispose()
+        {
+            TearDown();
+        }
     }
 }
