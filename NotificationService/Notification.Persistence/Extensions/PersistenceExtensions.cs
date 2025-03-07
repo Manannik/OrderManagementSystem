922e5e1561bf24746c66f597a3115b56d4e0ba44
zola191 <zola19@yandex.ru>
2025-03-07 19:27:52 +0300

    +

--- NotificationService/Notification.Persistence/Extensions/PersistenceExtensions.cs
+++ NotificationService/Notification.Persistence/Extensions/PersistenceExtensions.cs
@@ -17,7 +17,7 @@ public static class PersistenceExtensions
 
         services.AddDbContext<NotificationDbContext>(options =>
         {
-            options.UseNpgsql(configuration.GetConnectionString("NotificationServiceLocalConnectionString"));
+            options.UseNpgsql(configuration.GetConnectionString("NotificationServiceConnectionString"));
         });
         
         return services;
